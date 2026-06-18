917049d3965f24c149973d3b6871a5d73ad87903
Adrian Mursec <adrianmursec@Adrians-MacBook-Air.local>
2026-06-18 20:28:04 +0100

    add alias for line chart

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { App } from "vue";
 import LineChart from "./components/LineChart.vue";
 
-export { LineChart };
+export { LineChart, LineChart as AdiLineChart };
 
 export function install(app: App) {
   app.component("ADILineChart", LineChart);
