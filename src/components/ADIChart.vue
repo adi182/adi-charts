236682a579935499660f8a1ca26d51e1319647df
Adrian Mursec <adrianmursec@Adrians-MacBook-Air.local>
2026-06-19 17:10:15 +0100

    Added chart wrapper

---
+++ src/components/ADIChart.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { provide, ref } from 'vue'
+
+const props = defineProps({
+  data: {
+    type: Array,
+    default: () => [],
+  },
+  width: {
+    type: Number,
+    default: 640,
+  },
+  height: {
+    type: Number,
+    default: 400,
+  },
+  marginTop: {
+    type: Number,
+    default: 20,
+  },
+  marginRight: {
+    type: Number,
+    default: 20,
+  },
+  marginBottom: {
+    type: Number,
+    default: 30,
+  },
+  marginLeft: {
+    type: Number,
+    default: 40,
+  },
+})
+
+const adiChartData = ref({
+  data: props.data || [],
+})
+
+provide('adiChartData', adiChartData)
+</script>
+
+<template class="adi-charts__chart">
+  <slot />
+</template>
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { App } from "vue";
 import LineChart from "./components/LineChart.vue";
-
-export { LineChart, LineChart as AdiLineChart };
+import ADIChart from "./components/ADIChart.vue";
+export { LineChart, LineChart as AdiLineChart, ADIChart};
 
 export function install(app: App) {
   app.component("ADILineChart", LineChart);
+  app.component("ADIChart", ADIChart);
 }
 
 export const AdiCharts = { install };
